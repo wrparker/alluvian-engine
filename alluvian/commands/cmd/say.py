eb4c127f2423ff60cd9a6a5ad46ca82bfd621a65
wrparker <rparker@indeed.com>
2020-08-24 00:50:30 -0500

    add cyan color to say

--- alluvian/commands/cmd/say.py
+++ alluvian/commands/cmd/say.py
@@ -1,10 +1,17 @@
 from alluvian.commands.mud_command import MudCommand
 
+from alluvian.util.colors import Colors
+
 class Say(MudCommand):
 
     key = 'say'
     aliases = ['sa']
 
     def execute(self):
-        self.msg("You say: {}".format(self.arguments))
-        self.msg_room("{} says: {}".format(self.player.name, self.arguments))
+        self.msg("{}You say, '{}{}'.".format(Colors.fg.BCYAN,
+                                          self.arguments,
+                                          Colors.fg.RESET))
+        self.msg_room("{}{} says, '{}{}'.".format(Colors.fg.BCYAN,
+                                               self.player.name,
+                                               self.arguments,
+                                               Colors.fg.RESET))
