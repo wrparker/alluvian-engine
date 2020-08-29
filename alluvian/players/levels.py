cfdd7d5ba57b3bdb2ce145005883a5ec74d4e30e
wrparker <rparker@indeed.com>
2020-08-29 17:48:13 -0500

    Begin adding player levels.

---
+++ alluvian/players/levels.py
@@ -0,0 +1,8 @@
+from enum import IntEnum
+
+class Levels(IntEnum):
+    PLAYER = 33
+    IMM = 34
+    DEMIGOD = 35
+    GRGOD = 36
+    IMPL = 37
--- alluvian/players/models.py
+++ alluvian/players/models.py
@@ -6,6 +6,8 @@ class Player(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
+    level = models.IntegerField(default=1)
+
 
     def check_pw(self, password_input: str) -> bool:
         return bcrypt.checkpw(password_input.encode('utf-8'), self.password.encode('utf-8'))
